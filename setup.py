b45d52084718c8377ed699d3e85d6a89e0f7eeb4
Stefan Larsson <last@users.sourceforge.net>
2010-03-01 19:31:26 +0000

    Compiling for Windows as well.

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@
 # vim: set fileencoding=utf-8 :
 from setuptools import setup,find_packages,Extension
 import os.path
+import platform
+
+if platform.system() == 'Windows':
+    libraries = ['libmx', 'libmat', 'libeng']
+    include_dirs = ['C:/Program Files/MATLAB/R2008a/extern/include',
+                    'C:/Python26/Lib/site-packages/numpy/core/include']
+    library_dirs = ['C:/Program Files/MATLAB/R2008a/bin/win64',
+                    'C:/Program Files/MATLAB/R2008a/extern/lib/win64/microsoft']
+elif platform.system() == 'Linux':
+    libraries = ['eng', 'm', 'mx']
+    include_dirs = ['/opt/matlab/extern/include']
+    library_dirs = ['/opt/matlab/bin/glnxa64','/opt/matlab/bin/glnx86']
+else:
+    raise 'Unsupported system %s' % platform.system()
 
 setup(
         name='pymatlab',
@@ -18,13 +32,12 @@ setup(
                         'Operating System :: POSIX',
                         'Programming Language :: C',
                         'Programming Language :: Python',
-                        'Programming Language :: Python',
                           ],
         ext_modules=[Extension('pymatlab.matlab',
                       ['pymatlab/matlab.c'],
-                      libraries=['eng','m','mx'],
-                      include_dirs=['/opt/matlab/extern/include'],
-                      library_dirs=['/opt/matlab/bin/glnxa64','/opt/matlab/bin/glnx86'],
+                      libraries=libraries,
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       )],
         test_suite='tests',
         url = 'http://pymatlab.sourceforge.net/',
